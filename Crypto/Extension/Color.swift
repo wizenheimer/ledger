8a6297cfd4d950f307cd7dbea216d3646373c7cc
wizenheimer <xNayanKumar@gmail.com>
2025-05-24 19:43:54 +0530

    feat: add color theme

--- Crypto/Extension/Color.swift
+++ Crypto/Extension/Color.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("ColorGreen")
+    let red = Color("ColorRed")
+    let secondaryText = Color("SecondaryTextColor")
+}
