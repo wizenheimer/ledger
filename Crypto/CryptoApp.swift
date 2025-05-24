55c737d672714f8236e558c6108958f1491cbbb6
wizenheimer <xNayanKumar@gmail.com>
2025-05-24 19:13:26 +0530

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Nayan on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
