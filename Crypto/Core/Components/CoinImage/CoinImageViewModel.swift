ea80b87f6ebe15f5c9a5ddb516553122f6175483
wizenheimer <xNayanKumar@gmail.com>
2025-05-24 23:36:42 +0530

    feat: downloading coin images using combine, mvvm, and a networking layer

---
+++ Crypto/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  CoinImageViewModel.swift
+//  Crypto
+//
+//  Created by Nayan on 24/05/25.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    private let coin: CoinModel
+    private let dataService: CoinImageService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(coin: coin)
+        
+        self.isLoading = true
+        addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        dataService.$image
+            .sink { [weak self] _ in
+                self?.isLoading = false
+            } receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellables)
+
+    }
+    
+}
